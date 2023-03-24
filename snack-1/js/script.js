24543b951b38167fade242b9a36d55cd5faaa3bc
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-24 17:02:00 +0100

    snack 1 scaffolding

---
+++ snack-1/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * [X] Creare un array
+ * [X] All'interno dell'array creare 4 oggetti con due chiavi (nome e peso)
+ * [] Destrutturare i dati
+ * [] Trovare la il peso minore
+ * [] Stampare a schermo il nome e il peso della bici più leggera con template literal
+ */
+
+const bikesArray = [
+    {
+        bikeName: "Bianchi",
+        bikeWeight: 10
+    },
+    {
+        bikeName: "Rossi",
+        bikeWeight: 5
+    },
+    {
+        bikeName: "Verdi",
+        bikeWeight: 25
+    },
+    {
+        bikeName: "Neri",
+        bikeWeight: 800
+    }
+]
+
