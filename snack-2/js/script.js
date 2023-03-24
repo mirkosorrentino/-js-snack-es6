acc497ac26f0ca9f4e4ac79dd42d96cdc6ba5628
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-24 18:01:10 +0100

    snack 2 scaffolding

---
+++ snack-2/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * [] Creare un array
+ * [] All'interno degli array inserire oggetti rappresentanti delle sqaudre di calcio
+ * [] Le proprietà di tali oggetti dovranno essere nome, punti fatti, falli subiti
+ * [] La proprietà "nome" sarà da inserire da prompt, le altre caratteristiche sono da impostare a 0
+ * [] Generare numeri random al posto degli 0
+ * Usando la destrutturazione, creare un nuovo array in cui sono contenuti solo nome e falli subiti e stampare tutto in console
+ */
+
+const teamsArray = [
+    {
+        teamName,
+        points,
+        fouls
+    },
+    {
+        teamName,
+        points,
+        fouls
+    },
+    {
+        teamName,
+        points,
+        fouls
+    },
+    {
+        teamName,
+        points,
+        fouls
+    },
+]
+
+const teamName = prompt("Dimmi il nome di una squadra")
